b877aabdb551e268702d150f40991e638749a887
MarekWej <marekwejksznia17@gmail.com>
2023-04-17 15:11:59 +0200

    Changes

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
+
+
 import pygame
 
-pygame.init()                                  #inicjacja
+pygame.init()  # inicjacja
+window = pygame.display.set_mode((1280, 720))
 
-window = pygame.display.set_mode((1024,768))
+class Player:
+    def __init__(self):
+        self.x_cord = 0                                 #wpółrzędna x
+        self.y_cord = 0                                 #współrzędna y
+        self.width = 0                                  #szerokość
+        self.hight = 0                                  #wysokość
+        self.image = pygame.image.load("postać1.png")
 
-x = 0
-y = 0
-player = pygame.rect.Rect(x, y, 70, 70)        #Tworzenie postaci
+    def tick(self):                                     #wykonuje się raz na powtórznie pętli
+        pass
+    def draw(self):
+        window.blit(self.image,(self.x_cord, self.y_cord))
+
+def main():
+    run = True
+    player = Player()
+    while run:
+        pygame.time.Clock().tick(60)        #maks 60 fps
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:   # jeśli gracz zamknie okno
+                run = False
+        player.tick()
 
 
-run = True
-while run:
-    pygame.time.Clock().tick(60)     #zwalniamy prędkość postaci(FPS)
-    for event in pygame.event.get(): # zwracanie listy zdarzeń wywołanych przez gracza
-        if event.type == pygame.QUIT: # jeśli gracz zamknie okienko
-            run = False
+        window.fill((104, 191, 237))  # kolor tła
+        player.draw()
+        pygame.display.update()  # odświeżamy obraz
 
-    keys = pygame.key.get_pressed()    #Tworzymy ruch poprzez strzałki na klawiaturze
+        keys = pygame.key.get_pressed()  # Tworzymy ruch poprzez strzałki na klawiaturze
 
-    speed = 5
-
-    if keys[pygame.K_RIGHT]:            #strzałka w prawo
-        x += speed
-    if keys[pygame.K_LEFT]:             #strzałka w lewo
-        x -= speed
-    if keys[pygame.K_UP]:               #strzałka do góry
-        y -= speed
-    if keys[pygame.K_DOWN]:             #strzałka w dół
-        y += speed
-
-    player = pygame.rect.Rect(x, y, 70, 70)  #odświeżanie położenia postaci
-
-    window.fill((104, 191, 237))      #kolor tła
-    pygame.draw.rect(window,(20,200,20), player)   # wstawienie postaci
-    pygame.display.update()            #odświeżamy obraz
+if __name__ == "__main__":
+    main()
