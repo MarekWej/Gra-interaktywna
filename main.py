b98438b19a5e23decdc16cbb3ec00f16b828fd2a
MarekWej <marekwejksznia17@gmail.com>
2023-04-17 13:44:27 +0200

    changes

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import pygame
 
-pygame.init() # inicjacja
+pygame.init()                                  #inicjacja
 
-window = pygame.display.set_mode((800,600))
+window = pygame.display.set_mode((1024,768))
+
+x = 0
+y = 0
+player = pygame.rect.Rect(x, y, 70, 70)        #Tworzenie postaci
+
 
 run = True
 while run:
-        for event in pygame.event.get(): # zwracanie listy zdarzeń wywołanych przez gracza
-            if event.type == pygame.QUIT: # jeśli gracz zamknie okienko
-                run = False
+    pygame.time.Clock().tick(60)     #zwalniamy prędkość postaci(FPS)
+    for event in pygame.event.get(): # zwracanie listy zdarzeń wywołanych przez gracza
+        if event.type == pygame.QUIT: # jeśli gracz zamknie okienko
+            run = False
 
-        window.fill((104, 191, 237))      #kolor tła
-        pygame.display.update()            #
+    keys = pygame.key.get_pressed()    #Tworzymy ruch poprzez strzałki na klawiaturze
+
+    speed = 5
+
+    if keys[pygame.K_RIGHT]:            #strzałka w prawo
+        x += speed
+    if keys[pygame.K_LEFT]:             #strzałka w lewo
+        x -= speed
+    if keys[pygame.K_UP]:               #strzałka do góry
+        y -= speed
+    if keys[pygame.K_DOWN]:             #strzałka w dół
+        y += speed
+
+    player = pygame.rect.Rect(x, y, 70, 70)  #odświeżanie położenia postaci
+
+    window.fill((104, 191, 237))      #kolor tła
+    pygame.draw.rect(window,(20,200,20), player)   # wstawienie postaci
+    pygame.display.update()            #odświeżamy obraz
