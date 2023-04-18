5c2c4fb5aa94f71601fe25ab6d8a5e4a6babda36
MarekWej <marekwejksznia17@gmail.com>
2023-04-18 14:16:10 +0200

    changes

--- main.py
+++ main.py
@@ -4,33 +4,70 @@ from random import randint
 pygame.init()  # inicjacja
 window = pygame.display.set_mode((1280, 720))
 
-class Player:
+
+class Physic:
+    def __init__(self, x, y, width, height,  acc, max_vel):
+        self.x_cord = x                                #wpółrzędna x
+        self.y_cord = y                                #współrzędna y
+        self.hor_velocity = 0                           #prędkość w poziomie
+        self.ver_velocity = 0                           #prędkośc w pionie
+        self.acc = acc                                  #przyśpiesznie
+        self.max_vel = max_vel                          #maksymalna prędkość
+        self.width = width  # szerokość
+        self.hight = height  # wysokość
+        self.previous_x = x
+        self.previous_y = y
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
+
+    def physic_tick(self, beams):
+        self.ver_velocity += 0.7
+        self.x_cord += self.hor_velocity
+        self.y_cord += self.ver_velocity
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight) # odświeżanie hitboxów
+        for beam in beams:
+            if beam.hitbox.colliderect(self.hitbox):   #cofanie obiektu do miejsca z poprzedniej klatki
+                #self.x_cord = self.previous_x
+                self.y_cord = self.previous_y
+                self.ver_velocity = 0
+
+        self.previous_x = self.x_cord
+        self.previous_y = self.y_cord
+class Player(Physic):
     def __init__(self):
-        self.x_cord = 0                                 #wpółrzędna x
-        self.y_cord = 0                                 #współrzędna y
-        self.image = pygame.image.load("postać1.png")
-        self.width = self.image.get_width()                                 #szerokość
-        self.hight = self.image.get_height()                                  #wysokość
-        self.speed = 5                                  #prędkość postaci
-        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
+        self.image = pygame.image.load("john.png")
+        width = self.image.get_width()                                 #szerokość
+        hight = self.image.get_height()                                  #wysokość
+        super().__init__(0, 450, width, hight,  0.5, 5)
 
 
-    def tick(self, keys):                                     #wykonuje się raz na powtórznie pętli
-        if keys[pygame.K_w]:
-            self.y_cord -= self.speed
-        if keys[pygame.K_a]:
-            self.x_cord -= self.speed
-        if keys[pygame.K_s]:
-            self.y_cord += self.speed
-        if keys[pygame.K_d]:
-            self.x_cord += self.speed
 
-        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
+    def tick(self, keys, beams):                                   #wykonuje się raz na powtórznie pętli
+        self.physic_tick(beams)
+        if keys[pygame.K_a] and self.hor_velocity > self.max_vel * -1:
+            self.hor_velocity -= self.acc
+        if keys[pygame.K_d] and self.hor_velocity < self.max_vel:
+            self.hor_velocity += self.acc
+        if not (keys[pygame.K_d] or keys[pygame.K_a]):
+            if self.hor_velocity > 0:
+                self.hor_velocity -= self.acc
+            elif self.hor_velocity < 0:
+                self.hor_velocity += self.acc
 
     def draw(self):
         window.blit(self.image,(self.x_cord, self.y_cord))
 
-class Cash:
+
+class Beam:
+    def __init__(self, x, y, width, height):
+        self.x_cord = x
+        self.y_cord = y
+        self.width = width
+        self.height = height
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
+    def draw(self, window):
+        pygame.draw.rect(window, (128,128,128), self.hitbox)
+
+'''class Cash:
     def __init__(self):
         self.x_cord = randint(0,1280)
         self.y_cord = randint(0,720)
@@ -43,7 +80,7 @@ class Cash:
         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
 
     def draw(self):
-        window.blit(self.image,(self.x_cord,self.y_cord))
+        window.blit(self.image,(self.x_cord,self.y_cord))'''
 
 
 def main():
@@ -51,9 +88,10 @@ def main():
     run = True
     player = Player()
     clock = 0
-    score = 0
-    banknotes = []
     background = pygame.image.load("tło.png")
+    beams =[
+        Beam(7,563,1200,60)
+    ]
     while run:
         clock += pygame.time.Clock().tick(60)/1000        #maks 60 fps
         for event in pygame.event.get():
@@ -61,29 +99,17 @@ def main():
                 run = False
         keys = pygame.key.get_pressed()  # Tworzymy ruch poprzez strzałki na klawiaturze
 
-        if clock >= 2:
-            clock = 0
-            banknotes.append(Cash())
+        player.tick(keys, beams)
 
 
-        player.tick(keys)
-        for banknote in banknotes:
-            banknote.tick()
 
-
-        for banknote in banknotes:
-            if player.hitbox.colliderect(banknote.hitbox):
-                banknotes.remove(banknote)
-                score += 1
-
-
-        window.blit(background, (8, 0))         # kolor tła
+        window.blit(background, (0, 0))         # kolor tła
         player.draw()
-        for banknote in banknotes:
-            banknote.draw()
+        for beam in beams:
+            beam.draw(window)
+
         pygame.display.update()  # odświeżamy obraz
 
-    print(score)
 
 
 
