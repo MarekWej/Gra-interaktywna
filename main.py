bab3d5d85293d7725325a11dace8f31b33662c75
MarekWej <marekwejksznia17@gmail.com>
2023-04-18 11:55:05 +0200

    collection of bankots

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
-
 import pygame
+from random import randint
 
 pygame.init()  # inicjacja
 window = pygame.display.set_mode((1280, 720))
@@ -9,31 +8,84 @@ class Player:
     def __init__(self):
         self.x_cord = 0                                 #wpółrzędna x
         self.y_cord = 0                                 #współrzędna y
-        self.width = 0                                  #szerokość
-        self.hight = 0                                  #wysokość
         self.image = pygame.image.load("postać1.png")
+        self.width = self.image.get_width()                                 #szerokość
+        self.hight = self.image.get_height()                                  #wysokość
+        self.speed = 5                                  #prędkość postaci
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
 
-    def tick(self):                                     #wykonuje się raz na powtórznie pętli
-        pass
+
+    def tick(self, keys):                                     #wykonuje się raz na powtórznie pętli
+        if keys[pygame.K_w]:
+            self.y_cord -= self.speed
+        if keys[pygame.K_a]:
+            self.x_cord -= self.speed
+        if keys[pygame.K_s]:
+            self.y_cord += self.speed
+        if keys[pygame.K_d]:
+            self.x_cord += self.speed
+
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
+
     def draw(self):
         window.blit(self.image,(self.x_cord, self.y_cord))
 
+class Cash:
+    def __init__(self):
+        self.x_cord = randint(0,1280)
+        self.y_cord = randint(0,720)
+        self.image = pygame.image.load("banknot.png")
+        self.width = self.image.get_width()  # szerokość
+        self.hight = self.image.get_height() # wysokość
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
+
+    def tick(self):
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
+
+    def draw(self):
+        window.blit(self.image,(self.x_cord,self.y_cord))
+
+
 def main():
+    global keys
     run = True
     player = Player()
+    clock = 0
+    score = 0
+    banknotes = []
+    background = pygame.image.load("tło.png")
     while run:
-        pygame.time.Clock().tick(60)        #maks 60 fps
+        clock += pygame.time.Clock().tick(60)/1000        #maks 60 fps
         for event in pygame.event.get():
             if event.type == pygame.QUIT:   # jeśli gracz zamknie okno
                 run = False
-        player.tick()
+        keys = pygame.key.get_pressed()  # Tworzymy ruch poprzez strzałki na klawiaturze
+
+        if clock >= 2:
+            clock = 0
+            banknotes.append(Cash())
 
 
-        window.fill((104, 191, 237))  # kolor tła
+        player.tick(keys)
+        for banknote in banknotes:
+            banknote.tick()
+
+
+        for banknote in banknotes:
+            if player.hitbox.colliderect(banknote.hitbox):
+                banknotes.remove(banknote)
+                score += 1
+
+
+        window.blit(background, (8, 0))         # kolor tła
         player.draw()
+        for banknote in banknotes:
+            banknote.draw()
         pygame.display.update()  # odświeżamy obraz
 
-        keys = pygame.key.get_pressed()  # Tworzymy ruch poprzez strzałki na klawiaturze
+    print(score)
+
+
 
 if __name__ == "__main__":
     main()
