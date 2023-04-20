ab00ae787f014dfc919b3c08941b10f281d9a3a6
MarekWej <marekwejksznia17@gmail.com>
2023-04-20 14:17:57 +0200

    flip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from random import randint
+from math import floor
 
 pygame.init()  # inicjacja
 window = pygame.display.set_mode((1280, 720))
@@ -14,17 +15,17 @@ class Physic:
         self.acc = acc                                  #przyśpiesznie
         self.max_vel = max_vel                          #maksymalna prędkość
         self.width = width  # szerokość
-        self.hight = height  # wysokość
+        self.height = height  # wysokość
         self.previous_x = x
         self.previous_y = y
         self.jumping = False                            #czy postać skacze
-        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight)
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
 
     def physic_tick(self, beams):
         self.ver_velocity += 0.7
         self.x_cord += self.hor_velocity
         self.y_cord += self.ver_velocity
-        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.hight) # odświeżanie hitboxów
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height) # odświeżanie hitboxów
         for beam in beams:
             if beam.hitbox.colliderect(self.hitbox):   #cofanie obiektu do miejsca z poprzedniej klatki
                 if self.x_cord + self.width >= beam.x_cord + 1 > self.previous_x + self.width:  #kolizja z prawej strony
@@ -33,7 +34,7 @@ class Physic:
                 if self.x_cord <= beam.x_cord + beam.width - 1 < self.previous_x:  # kolizja z lewej strony
                     self.x_cord = self.previous_x
                     self.hor_velocity = 0
-                if self.y_cord + self.hight >= beam.y_cord + 1 > self.previous_y + self.hight:
+                if self.y_cord + self.height >= beam.y_cord + 1 > self.previous_y + self.height:
                     self.y_cord = self.previous_y
                     self.ver_velocity = 0
                     self.jumping = False
@@ -46,10 +47,17 @@ class Physic:
         self.previous_y = self.y_cord
 class Player(Physic):
     def __init__(self):
-        self.image = pygame.image.load("john.png")
-        width = self.image.get_width()                                 #szerokość
-        hight = self.image.get_height()                                  #wysokość
-        super().__init__(0, 450, width, hight,  0.5, 5)
+        self.stand_right_img = pygame.image.load("john.png")
+        self.stand_left_img = pygame.transform.flip(pygame.image.load("john.png"), True, False)        #odwracanie postaci
+        width = self.stand_right_img.get_width()                                                        #szerokość
+        height = self.stand_right_img.get_height()                                                       #wysokość
+        super().__init__(0, 450, width, height,  0.5, 5)
+        self.jump_right_img = pygame.image.load('jump.png')                                             #skok
+        self.jump_left_img = pygame.transform.flip(pygame.image.load('jump.png'), True, False)
+        self.walk_right_img = [pygame.image.load(f'walk/klatka0{x}.png') for x in range(1, 7)]
+        self.walk_left_img = [pygame.transform.flip(pygame.image.load(f'walk/klatka0{x}.png'), True, False) for x in range(1, 7)]
+        self.walk_index = 0
+        self.direction = 1
 
 
 
@@ -62,6 +70,10 @@ class Player(Physic):
         if keys[pygame.K_SPACE] and self.jumping is False:
             self.ver_velocity -= 15
             self.jumping = True
+        if self.hor_velocity > 0:
+            self.direction = 1
+        elif self.hor_velocity < 0:
+            self.direction = 0
         if not (keys[pygame.K_d] or keys[pygame.K_a]):
             if self.hor_velocity > 0:
                 self.hor_velocity -= self.acc
@@ -69,7 +81,25 @@ class Player(Physic):
                 self.hor_velocity += self.acc
 
     def draw(self):
-        window.blit(self.image,(self.x_cord, self.y_cord))
+        if self.jumping:
+            if self.direction == 0:
+                window.blit(self.jump_left_img, (self.x_cord, self.y_cord))
+            elif self.direction == 1:
+                window.blit(self.jump_right_img, (self.x_cord, self.y_cord))
+        elif self.hor_velocity != 0:
+            if self.direction == 0:
+                window.blit(self.walk_left_img[floor(self.walk_index)], (self.x_cord, self.y_cord))
+            elif self.direction == 1:
+                window.blit(self.walk_right_img[floor(self.walk_index)], (self.x_cord, self.y_cord))
+            self.walk_index += 1
+            if self.walk_index > 5:
+                self.walk_index = 0
+        else:
+            if self.direction == 0:
+                window.blit(self.stand_left_img,(self.x_cord, self.y_cord))
+            elif self.direction == 1:
+                window.blit(self.stand_right_img, (self.x_cord, self.y_cord))
+
 
 
 class Beam:
@@ -80,7 +110,7 @@ class Beam:
         self.height = height
         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.width, self.height)
     def draw(self, window):
-        pygame.draw.rect(window, (128,128,128), self.hitbox)
+        pygame.draw.rect(window, (128, 128, 128), self.hitbox)
 
 '''class Cash:
     def __init__(self):
